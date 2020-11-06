a6b9f2515c526b4b0dfa760091777b99c3eef448
Arash Suhaimi <arash.suhaimi@growthops.asia>
2020-11-07 02:17:49 +0800

    edit home page for heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "IMDB Movie Search" });
+  res.redirect('/movie/search');
 });
 
 module.exports = router;
